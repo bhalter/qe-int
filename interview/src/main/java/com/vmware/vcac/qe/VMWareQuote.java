6138d66cf9b5cc1263268af7dbbaa33379ec9c94
Will Wallace <wwallace@simplivt.local>
2016-10-02 19:15:12 -0400

    Initial commit

---
+++ interview/src/main/java/com/vmware/vcac/qe/VMWareQuote.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package com.vmware.vcac.qe;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Gather stock quotes for VMW from two sources on a schedule (every 5 minutes).  
+ * Print and compare the results. 
+ * @author willwallace
+ */
+public class VMWareQuote {
+	private static Log LOG = LogFactory.getLog(VMWareQuote.class);
+
+	/**
+	 * main: Entry point.
+	 * @param args: not used
+	 * @throws InterruptedException|IOException
+	 */
+	public static void main(String[] args) {
+		final RestClient client = new RestClient();
+		try {
+			for (;;) {
+				final java.util.List<Quotable> quotes = new ArrayList<Quotable>();
+				quotes.add(new YahooFinance());
+				quotes.add(new MarketOnDemand());
+				
+				
+				for (final Quotable quote : quotes) {
+					client.execute(quote);
+					LOG.info(String.format("Quote: %f", quote.getQuote()));
+				}
+				
+				for (final Quotable quote : quotes) {
+					LOG.info(quote.getReport());
+				}
+				LOG.info(String.format("Difference=%f",CalculateDifference(
+						quotes.get(0).getQuote(), 
+						quotes.get(1).getQuote())));
+				
+				Thread.sleep(TimeUnit.MINUTES.toMillis(5L));
+			}
+		} catch (IOException|InterruptedException e) {
+			LOG.info(String.format("Caught %s. %s", e.getClass().getName(), e.getMessage()));
+		} finally {
+			try {
+				client.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+/**
+ * Calculate the difference between two floats
+ * @param f1 first float value
+ * @param f2 second float value
+ * @return difference
+ */
+	public static float CalculateDifference(final float f1, final float f2) {
+		return Math.abs(f1 - f2);
+	}
+
+}
---
+++ interview/src/main/java/com/vmware/vcac/qe/YahooFinance.java
@@ -0,0 +1,52 @@
+package com.vmware.vcac.qe;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+
+/**
+ * An implementation of the Quotable interface for the YahooFinance site.
+ * @author willwallace
+ *
+ */
+public class YahooFinance implements Quotable {
+
+	float quote = 0.0F;
+	final String name = "Yahoo";
+	
+	@Override
+	public HttpUriRequest getRequest() {
+		return new HttpGet("http://finance.yahoo.com:80/webservice/v1/symbols/VMW/quote?format=json");
+	}
+
+	@Override
+	public void process(String payload) {
+		//TODO Use a json parser to parse
+//		final Gson gson = new GsonBuilder().create();
+//		final YahooWrapper wrapper = gson.fromJson(payload, YahooWrapper.class);
+//		final List list = wrapper.getList();
+//		quote = wrapper.getList().getResources()[0].getFields().getPrice();
+		
+		//Workaround
+		String s = "\"price\" : \"";
+		final int start = payload.indexOf(s) + s.length();
+		final int end = payload.indexOf("\",",start);
+		final String value = payload.substring(start, end);
+		quote = Float.parseFloat(value);
+	}
+
+	@Override
+	public float getQuote() {
+		return quote;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String getReport() {
+		return String.format("%s=%s",getName(), quote);
+	}
+
+}
